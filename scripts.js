61e7ec8827e9615ddba91f7cb606eb258b7ef4bb
takumi212 <101326931+takumi212@users.noreply.github.com>
2023-02-01 22:55:20 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@
 $(function() {
     $("header").children(".outline2,h5").hide();
     $("header").children(".outline1").show();
-    $('#jbody').click(function() {
+    $("header").click(function() {
             // $("header,p").hide(2000);
             $("header").children(".outline2,h5").show(500); 
             $("header").children(".outline1").hide(2000);

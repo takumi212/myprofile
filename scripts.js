c0d464b30d4378da06534b025486438a1ca0c44b
takumiengineer <101326931+takumi212@users.noreply.github.com>
2023-04-15 01:39:08 +0900

    Update scripts.js
    I changed my jQuery's code.

--- scripts.js
+++ scripts.js
@@ -7,17 +7,45 @@
 // [scripts.js]を使用している。
 
 // headerにクリックすると3か4行の文章が表示されるようにする。
-
+// 現在はこちらを使用している。
 $(function() {
-    $("header").children(".outline2,h5").hide();
-    $("header").children(".outline1").show();
-    $("header").click(function() {
-            // $("header,p").hide(2000);
-            $("header").children(".outline2,h5").show(500); 
-            $("header").children(".outline1").hide(2000);
-        }
-    );
+  $("header").children(".outline2,h5").hide();
+  $("header").children(".outline1").show();
+  $("header").click(function() {
+          $("header").children(".outline2,h5").toggle(2000); 
+          $("header").children(".outline1").toggle(2000);
 });
+});
+
+// $(function() {
+//   $("header").children(".outline1").hide();
+//   $("header").children(".outline2,h5").show();
+//   $("header").click(function() {
+//           $("header").children(".outline1").show(2000); 
+//           $("header").children(".outline2,h5").hide(500);
+// });
+// });
+
+
+
+
+// クリックすると、『.outline2,h5"』が隠されて、『.outline1"』が表示される仕組みになる。
+
+// $(function() {
+
+//   $("header").children(".outline2,h5").hide();
+//   $("header").children(".outline1").show();
+  
+//   $("header").click(function() {
+//   // ".outline2" と ".outline2,h5" を隠す
+//   $("header").children(".outline2, h5").hide();
+//   // ".outline1" を表示する
+//   $("headers").children(".outline1").show();
+//   });
+  
+//   });
+
+
 
 // 背景に泡が流れているようにしている。
 

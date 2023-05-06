84e042816a309d664868bc9865cdb7d4f32f43af
takumiengineer <101326931+takumi212@users.noreply.github.com>
2023-05-06 18:40:50 +0900

    Update scripts.js
    I changed my scripts.js.

--- scripts.js
+++ scripts.js
@@ -8,42 +8,34 @@
 
 // headerにクリックすると3か4行の文章が表示されるようにする。
 // 現在はこちらを使用している。
+// $(function() {
+//   $("header").children(".outline2,h5").hide();
+//   $("header").children(".outline1").show();
+//   $("header").click(function() {
+//           $("header").children(".outline2,h5").toggle(2000); 
+//           $("header").children(".outline1").toggle(2000);
+// });
+// });
+
+
+
+
 $(function() {
   $("header").children(".outline2,h5").hide();
   $("header").children(".outline1").show();
   $("header").click(function() {
-          $("header").children(".outline2,h5").toggle(2000); 
-          $("header").children(".outline1").toggle(2000);
+    if ($("header").children(".outline1").is(":visible")) {
+      $("header").children(".outline1").fadeOut(1000, function() {
+        $("header").children(".outline2,h5").fadeIn(1000);
+      });
+    } else {
+      $("header").children(".outline2,h5").fadeOut(1000, function() {
+        $("header").children(".outline1").fadeIn(1000);
+      });
+    }
+  });
 });
-});
 
-// $(function() {
-//   $("header").children(".outline1").hide();
-//   $("header").children(".outline2,h5").show();
-//   $("header").click(function() {
-//           $("header").children(".outline1").show(2000); 
-//           $("header").children(".outline2,h5").hide(500);
-// });
-// });
-
-
-
-
-// クリックすると、『.outline2,h5"』が隠されて、『.outline1"』が表示される仕組みになる。
-
-// $(function() {
-
-//   $("header").children(".outline2,h5").hide();
-//   $("header").children(".outline1").show();
-  
-//   $("header").click(function() {
-//   // ".outline2" と ".outline2,h5" を隠す
-//   $("header").children(".outline2, h5").hide();
-//   // ".outline1" を表示する
-//   $("headers").children(".outline1").show();
-//   });
-  
-//   });
 
 
 
@@ -59,9 +51,3 @@ $(function() {
   
 
 // 僕の写真が出現する
-
- 
-
-
-
-

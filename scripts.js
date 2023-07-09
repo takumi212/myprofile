7fd3a39b8bc12e5bea72d35fa858b6ec00037421
takumiengineer <101326931+takumi212@users.noreply.github.com>
2023-07-10 07:39:22 +0900

    Update scripts.js
    I changed my jQuery.

--- scripts.js
+++ scripts.js
@@ -2,21 +2,6 @@
 'use strict';
 
 
-// ここから切る。
-
-// [scripts.js]を使用している。
-
-// headerにクリックすると3か4行の文章が表示されるようにする。
-// 現在はこちらを使用している。
-// $(function() {
-//   $("header").children(".outline2,h5").hide();
-//   $("header").children(".outline1").show();
-//   $("header").click(function() {
-//           $("header").children(".outline2,h5").toggle(2000); 
-//           $("header").children(".outline1").toggle(2000);
-// });
-// });
-
 
 
 
@@ -39,15 +24,27 @@ $(function() {
 
 
 
+
+
+
 // 背景に泡が流れているようにしている。
 
-  bubbly({
-    gradientStart: '#fff4e6',
-    gradientStop: '#ffe9e4',
-    shadowBlur: 1,
-    compose: 'source-over',
-    fillFunc: () => `hsla(${Math.random() * 50}, 100%, 50%, .3)`,
-  });
+bubbly({
+  gradientStart: '#fff4e6',
+  gradientStop: '#ffe9e4',
+  shadowBlur: 1,
+  compose: 'source-over',
+  fillFunc: () => `hsla(${Math.random() * 50}, 100%, 50%, .3)`,
+});
   
 
 // 僕の写真が出現する
+
+
+
+
+
+
+
+
+
